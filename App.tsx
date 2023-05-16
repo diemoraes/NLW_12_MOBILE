e1fa72ce94c0c5519af71ca4848cde2568691645
diego.moraes <diego.moraes@spsconsultoria.com.br>
2023-05-16 20:27:18 -0300

    Ajuste

--- App.tsx
+++ App.tsx
@@ -5,7 +5,7 @@ export default function App() {
   return (
     <View className="bg-gray-950 flex-1 items-center justify-center">
       <Text className="text-zinc-50 font-bold text-5xl">
-        Hello Diego Moraes
+        Hello Diego Moraes!
       </Text>
       <StatusBar style="light" />
     </View>
